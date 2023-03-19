7f20e84f2d10c2b64c7c5d421e66a00603927ae5
cosmo <laurellicosmo@gmail.com>
2023-03-19 09:21:50 +0100

    adds comment in intro

--- arcpy_00_intro.py
+++ arcpy_00_intro.py
@@ -1,14 +1,17 @@
 # import the arcpy library
 import arcpy
 
+# Set env for overwriting feature
 arcpy.env.overwriteOutput = True
+# Set the env default GDB
 arcpy.env.workspace = r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data\Sample.gdb"
 
 fc = r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data\ne_10m_admin_0_countries.shp"
 numFeats = arcpy.GetCount_management(fc)
 print("{0} has {1} feature(s)".format(fc, numFeats))
-
-arcpy.CreateFileGDB_management(r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data", "Sample")
+# Create a new GDB
+arcpy.CreateFileGDB_management(
+    r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data", "Sample")
 arcpy.Select_analysis(fc, "Egypt", "NAME = 'Egypt'")
 
 print("Script completed!")
