315a80045b1473dc1d5c69024ea5180b9056b355
cosmo <laurellicosmo@gmail.com>
2023-03-19 09:44:51 +0100

    add comments to intro

--- arcpy_00_intro.py
+++ arcpy_00_intro.py
@@ -5,7 +5,7 @@ import arcpy
 arcpy.env.overwriteOutput = True
 # Set the env default GDB
 arcpy.env.workspace = r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data\Sample.gdb"
-
+# pointer to a shp file
 fc = r"D:\00-Learning\02-GisDev\01-ArcPy\LPA\Data\ne_10m_admin_0_countries.shp"
 numFeats = arcpy.GetCount_management(fc)
 print("{0} has {1} feature(s)".format(fc, numFeats))
